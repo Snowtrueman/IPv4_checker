36434519199fa84d1b23057c6298e3ae4ddc9539
snowtrueman <snowtrueman@yandex.ru>
2023-07-06 00:07:16 +0300

    New feature added. Now you can specify an arbitrary octet length in bits.

--- ip.py
+++ ip.py
@@ -1,18 +1,41 @@
-import re
-
-
 class IP:
     """
-    This class represents an IP address as four binary octets.
+    This class represents an IP address as four binary octets with custom octet length.
     """
 
-    def __init__(self, ipv4_address: str):
-        self._ip_pattern = r"^(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)(?:\.(?!$)|$)){4}$"
-        self._ip = self._get_binary_octets(ipv4_address)
+    def __init__(self, ipv4_address: str, number_of_bits: int = 8, is_mask: bool = False):
+        """
+        The initial method.
+        Args:
+            ipv4_address: The network address as a string.
+            number_of_bits: The length of octet in bits.
+            is_mask: Flag for network mask determination.
+        Attributes:
+            self._number_of_bits: The length of octet in bits.
+            self._is_mask: Flag for network mask determination.
+            self._str_ip: The IPv4 string representation.
+            self._valid_mask_values: The valid octets values for the netmask.
+            self._ip: The IPv4 binary representation.
+        """
+
+        self._number_of_bits = number_of_bits
+        self._is_mask = is_mask
         self._str_ip = ipv4_address
+        self._valid_mask_values = self._generate_valid_mask_values()
+        self._ip = self._get_binary_octets(ipv4_address)
 
     def __str__(self):
         return self._str_ip
+
+    def _generate_valid_mask_values(self) -> list:
+        """
+        A getter method that returns IPv4 address as a list of binary octets.
+
+        Returns:
+            The list of binary octets.
+        """
+
+        return [2 ** self._number_of_bits - 2 ** i for i in range(self._number_of_bits + 1)]
 
     def _get_binary_octets(self, ipv4_address: str) -> list:
         """
@@ -25,8 +48,16 @@ class IP:
         Raises:
             ValueError: If the format does not match.
         """
-
-        if re.match(self._ip_pattern, ipv4_address):
+        _flag = True
+        ipv4_splitted = ipv4_address.split(".")
+        for octet in ipv4_splitted:
+            if not self._is_mask:
+                if int(octet) not in range(0, 128):
+                    _flag = False
+            else:
+                if int(octet) not in self._valid_mask_values:
+                    _flag = False
+        if _flag:
             return [bin(int(octet)) for octet in ipv4_address.split(".")]
         else:
             raise ValueError
--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from ip_checker import IPChecker
 
 
 if __name__ == "__main__":
+    number_of_bits = input("Please enter the length of one octet in bits: ")
     network = input("Please enter the IPv4 network address: ")
     mask = input("Please enter the network mask: ")
     input_file = input("Please provide the list if IPs: ")
     try:
-        binary_network = IP(network)
-        binary_mask = IP(mask)
+        number_of_bits = int(number_of_bits)
+        binary_network = IP(network, number_of_bits=7)
+        binary_mask = IP(mask, number_of_bits=7, is_mask=True)
         result = IPChecker(binary_network, binary_mask, input_file)
         result.check()
     except ValueError:
-        print(f"Your network address ({network}) or network mask ({mask}) are not valid")
+        print(f"Your network address ({network}), network mask ({mask}) or octet length are not valid")
     except FileNotFoundError:
         print(f"There is no such file or incorrect file type: {input_file}")
     except EnvironmentError:
